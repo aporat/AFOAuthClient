c02dbf1feb5ab3db050b7027377ee2db12252715
Adar Porat <adar.porat@gmail.com>
2014-06-11 08:09:58 -0400

    added example project

---
+++ example/example/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  example
+//
+//  Created by Adar Porat on 5/29/14.
+//  Copyright (c) 2014 Adar Porat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ example/example/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  example
+//
+//  Created by Adar Porat on 5/29/14.
+//  Copyright (c) 2014 Adar Porat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
